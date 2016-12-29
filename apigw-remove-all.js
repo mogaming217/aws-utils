986a9eb58efbba8726113a8bd459881009f2b4c4
最上聖也 <mogami@hi.cis.kit.ac.jp>
2016-12-29 13:59:14 +0900

    apigw-createに併せて微修正

--- apigw-remove-all.js
+++ apigw-remove-all.js
@@ -2,31 +2,31 @@
 
 let AWS = require("aws-sdk");
 let config = require("./config");
-config.region = "ap-northeast-1";
+config.region = "ap-northeast-1"; // 適宜変更
 AWS.config.update(config);
 let apigw = new AWS.APIGateway();
 
-apigw.getRestApis({}, function(err, data){
-  if(err){
-    console.log(err, err.stack);
-  }else{
-    let i = 0;
-    let length = data.items.length;
-    let id = setInterval(function(){
-      if(i < length){
-        let param = {
-          restApiId: data.items[i++].id
-        };
-        apigw.deleteRestApi(param, function(err, data){
-          if(err){
-            console.log(err);
-          }
-        });
-        if(i >= length){
-            clearInterval(id);
-            console.log("done");
-        }
-      }
-    }, 61000);
-  }
+apigw.getRestApis({}, function(err, data) {
+    if (err) {
+        console.log(err, err.stack);
+    } else {
+        let i = 0;
+        let length = data.items.length;
+        let id = setInterval(function() {
+            let item = data.items[i++];
+            let param = {
+                restApiId: item.id
+            };
+            apigw.deleteRestApi(param, function(err, data) {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(item.name + " deleted");
+                if (i >= length) {
+                    clearInterval(id);
+                    console.log("done");
+                }
+            });
+        }, 61000);
+    }
 });
